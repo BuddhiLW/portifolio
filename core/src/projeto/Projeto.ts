f0e8b51e067398f99838d89289d2ca64540de841
Pedro Gomes <pedrogbranquinho@gmail.com>
2025-03-20 14:12:06 -0300

    feat: core models completed

--- core/src/projeto/Projeto.ts
+++ core/src/projeto/Projeto.ts
@@ -3,12 +3,12 @@ import { Nivel } from "./Nivel"
 import { Tipo } from "./Tipo"
 
 export default interface Projeto {
-  id: number
-  nome: string
-  descricao: string
-  imagens: string
-  repositorio: string
-  tecnologias: Tecnologia[]
-  nivel: Nivel
-  tipo: Tipo
+	id: number
+	nome: string
+	descricao: string
+	imagens: string
+	repositorio: string
+	tecnologias: Tecnologia[]
+	nivel: Nivel
+	tipo: Tipo
 }
